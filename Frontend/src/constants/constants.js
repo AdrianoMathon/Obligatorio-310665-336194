1bd878a4471066fe03793f2df416f95375d03b97
AdrianoMathon <adrianomathon@hotmail.com>
2025-11-07 01:05:09 -0300

    inicio proyecto

---
+++ Frontend/src/constants/constants.js
@@ -0,0 +1,1 @@
+export const urlBackend = "https://obligatorio-310665-336194.vercel.app/api/v1";
---
+++ Frontend/src/routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+
+const Rutas = () => {
+  return (
+    <BrowserRouter>
+      <GlobalSpinner />
+      <Routes>
+      
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Rutas;
